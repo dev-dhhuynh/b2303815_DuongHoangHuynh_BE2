9710cda1c325ca9f64497b496a1e5ed5f2b89394
Hoang Huynh <dhhuynh.tvb2020@gmail.com>
2026-01-22 22:50:55 +0700

    Cap nhat cac handler

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -2,33 +2,6 @@ const ApiError = require("../api-error");
 const MongoDB = require("../utils/mongodb.util");
 const ContactService = require("../services/contact.service");
 
-exports.create = (req, res) => {
-  res.send({ message: "create handler" });
-};
-
-exports.findAll = (req, res) => {
-  res.send({ message: "findAll handler" });
-};
-exports.findOne = (req, res) => {
-  res.send({ message: "findOne handler" });
-};
-
-exports.update = (req, res) => {
-  res.send({ message: "update handler" });
-};
-
-exports.delete = (req, res) => {
-  res.send({ message: "delete handler" });
-};
-
-exports.deleteAll = (req, res) => {
-  res.send({ message: "deleteAll handler" });
-};
-
-exports.findAllFavorite = (req, res) => {
-  res.send({ message: "findAllFavorite handler" });
-};
-
 exports.create = async (req, res, next) => {
   if (!req.body?.name) {
     return next(new ApiError(400, "Name can not be empty"));
